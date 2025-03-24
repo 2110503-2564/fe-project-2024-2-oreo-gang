03dc067b24319457df0452f68c50d5374c14d581
Soravit Polpraprut <ten7009@gmail.com>
2025-03-25 03:38:52 +0700

    Seperated Footer into another Component, fixed ViewBooking page using pxs instead of vw,vh

--- src/app/Viewbooking/page.tsx
+++ src/app/Viewbooking/page.tsx
@@ -1,9 +1,11 @@
-import { View } from "@/components/Viewbooking";
+import OreoFooter from "@/components/OreoFooter";
+import  View  from "@/components/Viewbooking";
 
 export default function Views() {
   return (
     <main >
-      <View />
+      <View/>
+      <OreoFooter/>
     </main>
   );
 }
